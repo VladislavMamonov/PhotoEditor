04f5da0b37353b279274bd506af8aa917e10f3f4
Vladislav Mamonov <VMwork0123@gmail.com>
2020-05-02 23:49:44 +0700

    done?

--- editor.hpp
+++ editor.hpp
@@ -1,13 +1,18 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/core/core.hpp>
+#include <core/mat.hpp>
 #include <iostream>
 #include <unistd.h>
 
 using namespace std;
+using namespace cv;
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#define STBIW_ZLIB_COMPRESS
 
 
 struct image
@@ -52,7 +57,7 @@ int convert_to_gray(image &img)
   int gray_channels = img.channels == 4 ? 2 : 1;
 
   if (gray_img == nullptr) {
-    cout << "Unable to allocate memory for the gray image" << endl;
+    cout << "Unable to allocate memory for the image" << endl;
     return 1;
   }
 
@@ -65,7 +70,7 @@ int convert_to_gray(image &img)
   }
 
   if (img.format == "jpg")
-    stbi_write_jpg("gray.jpg", img.width, img.height, gray_channels, gray_img, 0);
+    stbi_write_jpg("gray.jpg", img.width, img.height, gray_channels, gray_img, 100);
 
   if (img.format == "png")
     stbi_write_png("gray.png", img.width, img.height, gray_channels, gray_img, img.width * gray_channels);
@@ -80,7 +85,7 @@ int convert_to_sepia(image &img)
   unsigned char *sepia_img = (unsigned char *)malloc(img.size);
 
   if (sepia_img == nullptr) {
-    cout << "Unable to allocate memory for the sepia image" << endl;
+    cout << "Unable to allocate memory for the image" << endl;
     return 1;
   }
 
@@ -95,13 +100,118 @@ int convert_to_sepia(image &img)
     }
 
   if (img.format == "jpg")
-    stbi_write_jpg("sepia.jpg", img.width, img.height, img.channels, sepia_img, 0);
+    stbi_write_jpg("sepia.jpg", img.width, img.height, img.channels, sepia_img, 100);
 
   if (img.format == "png")
     stbi_write_png("sepia.png", img.width, img.height, img.channels, sepia_img, img.width * img.channels);
 
 
   free(sepia_img);
+  return 0;
+}
+
+
+int convert_to_negative(image &img)
+{
+  unsigned char *negative_img = (unsigned char *)malloc(img.size);
+
+  if (negative_img == nullptr) {
+    cout << "Unable to allocate memory for the image" << endl;
+    return 1;
+  }
+
+  for (unsigned char *p = img.img, *pg = negative_img; p != img.img + img.size;
+     p += img.channels, pg += img.channels) {
+       *pg = (uint8_t)(255.0 - *p);
+       *(pg + 1) = (uint8_t)(255.0 - *(p + 1));
+       *(pg + 2) = (uint8_t)(255.0 - *(p + 2));
+      if (img.channels == 4) {
+        *(pg + 3) = *(p + 3);
+      }
+    }
+
+  if (img.format == "jpg")
+    stbi_write_jpg("negative.jpg", img.width, img.height, img.channels, negative_img, 100);
+
+  if (img.format == "png")
+    stbi_write_png("negative.png", img.width, img.height, img.channels, negative_img, img.width * img.channels);
+
+
+  free(negative_img);
+  return 0;
+}
+
+
+int sharpening(image &img, const string file_name)
+{
+  Mat userImg, sharpening_img;
+  userImg = imread(file_name, 1);
+  sharpening_img = imread(file_name, 1);
+
+  if (userImg.empty()) {
+    cout << "Unable to allocate memory for the image" << endl;
+    return 1;
+  }
+
+  float kernel[9];
+  kernel[0] = -1;
+  kernel[1] = -1;
+  kernel[2] = -1;
+
+  kernel[3] = -1;
+  kernel[4] = 9;
+  kernel[5] = -1;
+
+  kernel[6] = -1;
+  kernel[7] = -1;
+  kernel[8] = -1;
+
+  Mat kernel_matrix = Mat(3, 3, CV_32FC1, kernel);
+  filter2D(userImg, sharpening_img, -1, kernel_matrix, Point(-1, -1), 0, BORDER_DEFAULT);
+
+  if (img.format == "jpg")
+    imwrite("sharpening.jpg", sharpening_img);
+
+  if (img.format == "png")
+    imwrite("sharpening.png", sharpening_img);
+
+  return 0;
+}
+
+
+int convert_to_emboss(image &img, const string file_name)
+{
+  Mat userImg, aquarelle_img;
+  userImg = imread(file_name, 1);
+  aquarelle_img = imread(file_name, 1);
+
+  if (userImg.empty()) {
+    cout << "Unable to allocate memory for the image" << endl;
+    return 1;
+  }
+
+  float kernel[9];
+  kernel[0] = -2;
+  kernel[1] = -1;
+  kernel[2] = 0;
+
+  kernel[3] = -1;
+  kernel[4] = 1;
+  kernel[5] = 1;
+
+  kernel[6] = 0;
+  kernel[7] = 1;
+  kernel[8] = 2;
+
+  Mat kernel_matrix = Mat(3, 3, CV_32FC1, kernel);
+  filter2D(userImg, aquarelle_img, -1, kernel_matrix, Point(-1, -1), 0, BORDER_DEFAULT);
+
+  if (img.format == "jpg")
+    imwrite("emboss.jpg", aquarelle_img);
+
+  if (img.format == "png")
+    imwrite("emboss.png", aquarelle_img);
+
   return 0;
 }
 
@@ -113,19 +223,74 @@ int image_quality(image &img)
     return 1;
   }
 
-  //Добавить готовые пресеты
 
   int quality_value;
-  cout << "quality value[1; 100]: ";
-  cin >> quality_value;
+  int selection;
+
+  cout << "1. Worst quality[1]" << endl;
+  cout << "2. Bad quality[25]" << endl;
+  cout << "3. Satisfactory quality[50]" << endl;
+  cout << "4. Slight compression[85]" << endl;
+  cout << "5. Increase quality[100]" << endl;
+  cout << "6. Set quality manually" << endl;
+
+  cout << endl << "selection: ";
+  cin >> selection;
+
+  switch (selection) {
+    case 1:
+      quality_value = 1;
+      break;
+    case 2:
+      quality_value = 25;
+      break;
+    case 3:
+      quality_value = 50;
+      break;
+    case 4:
+      quality_value = 85;
+      break;
+    case 5:
+      quality_value = 100;
+      break;
+    case 6:
+      cout << "quality value[1; 100]: ";
+      cin >> quality_value;
+      break;
+    default:
+      quality_value = 100;
+      break;
+  }
 
   if (img.format == "jpg")
     stbi_write_jpg("compression.jpg", img.width, img.height, img.channels, img.img, quality_value);
 
   if (img.format == "png") {
-    stbi_write_png_compression_level = 40;
     stbi_write_png("compression.png", img.width, img.height, img.channels, img.img, img.width * img.channels);
   }
+
+  return 0;
+}
+
+
+int image_flip(image &img, const char *img_name)
+{
+  if (img.img == NULL) {
+    cout << "Image upload error" << endl;
+    return 1;
+  }
+
+  stbi_set_flip_vertically_on_load(1);
+  img.img = stbi_load(img_name, &img.width, &img.height, &img.channels, 0);
+
+  if (img.format == "jpg")
+    stbi_write_jpg("flip.jpg", img.width, img.height, img.channels, img.img, 100);
+
+  if (img.format == "png")
+    stbi_write_png("flip.png", img.width, img.height, img.channels, img.img, img.width * img.channels);
+
+  stbi_set_flip_vertically_on_load(0);
+  img.img = stbi_load(img_name, &img.width, &img.height, &img.channels, 0);
 
   return 0;
 }
--- interface.cpp
+++ interface.cpp
@@ -15,46 +15,81 @@ void interface()
     cout << "_______________" << endl << endl;
 
     cout << "1. Load image" << endl;
-    cout << "2. Gray Filter" << endl;
-    cout << "3. Sepia Filter" << endl;
-    cout << "4. Quality settings" << endl;
+    cout << "2. Gray filter" << endl;
+    cout << "3. Sepia filter" << endl;
+    cout << "4. Negative filter" << endl;
+    cout << "5. Sharpening" << endl;
+    cout << "6. Emboss filter" << endl;
+    cout << "7. Quality settings(jpg only)" << endl;
+    cout << "8. Image rotation" << endl;
     cout << "Any key to exit" << endl << endl;
 
     cout << "Selection: ";
     cin >> selection;
 
-    if (selection == 1) {
-      string str;
-      cout << "File name: ";
-      cin >> str;
+    const char *file_name;
+    static string str;
 
-      const char *file_name = str.c_str();
-      load_image(img, file_name);
+    switch (selection)
+    {
+      case 1: {
+        cout << "File name: ";
+        cin >> str;
 
-      sleep(2);
-      system("clear");
+        file_name = str.c_str();
+        load_image(img, file_name);
+
+        sleep(2);
+        system("clear");
+        break;
+      }
+
+      case 2:
+        convert_to_gray(img);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 3:
+        convert_to_sepia(img);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 4:
+        convert_to_negative(img);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 5:
+        sharpening(img, str);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 6:
+        convert_to_emboss(img, str);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 7:
+        image_quality(img);
+        sleep(2);
+        system("clear");
+        break;
+
+      case 8:
+        image_flip(img, file_name);
+        sleep(2);
+        system("clear");
+        break;
+
+      default:
+        isExit = true;
+        break;
     }
-
-    if (selection == 2) {
-      convert_to_gray(img);
-      sleep(2);
-      system("clear");
-    }
-
-    if (selection == 3) {
-      convert_to_sepia(img);
-      sleep(2);
-      system("clear");
-    }
-
-    if (selection == 4) {
-      image_quality(img);
-      sleep(2);
-      system("clear");
-    }
-
-    if (selection < 1 || selection > 4)
-      isExit = true;
   }
 }
 
